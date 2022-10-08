0bab7707bf286d7cf2dbc799fcd29178468ca61a
Jiohim <bohdankrasin@gmail.com>
2022-10-08 18:17:24 +0200

    add initial project

---
+++ src/main/java/pl/jiohim/ditest/DiTestApplication.java
@@ -0,0 +1,13 @@
+package pl.jiohim.ditest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jiohim/ditest/DiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jiohim.ditest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
